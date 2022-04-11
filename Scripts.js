3ab63e00c8962606b7645c4fcc7850ddd7941ff1
TLST1 <103438328+TLST1@users.noreply.github.com>
2022-04-11 22:25:08 +0200

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -2,7 +2,7 @@
 function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
-delay(1000).then(() => {
+delay(3100).then(() => {
     const toasts = document.querySelectorAll('.toast-root');
     toasts.forEach(toast => toast.remove());
     console.log("Warning toast removed!");
