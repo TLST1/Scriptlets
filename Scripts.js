a5097bc3013d0737cdaf72cf4179762d2004d5bd
TLST1 <103438328+TLST1@users.noreply.github.com>
2022-04-11 22:41:30 +0200

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -1,9 +1,12 @@
 /// RemoveWarningToast.js
-function delay(time) {
-    return new Promise(resolve => setTimeout(resolve, time));
-}
-delay(3100).then(() => {
+const myInterval = setInterval(ToastClean, 100);
+function ToastClean() {
     const toasts = document.querySelectorAll('.toast-root');
-    toasts.forEach(toast => toast.remove());
-    console.log("Warning toast removed!");
-});
+    if (toasts.length > 0) {
+        toasts.forEach(toast => toast.remove());
+        console.log("Warning toast removed!");
+        clearInterval(myInterval);
+    } else {
+        console.log("tick");
+    }
+}
