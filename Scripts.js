8eb9bfb6eb2127bdeaa46c2fee35bac1b078d39f
TLST1 <103438328+TLST1@users.noreply.github.com>
2022-04-11 22:46:44 +0200

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -1,12 +1,21 @@
-/// RemoveWarningToast.js
-const myInterval = setInterval(ToastClean, 100);
-function ToastClean() {
-    const toasts = document.querySelectorAll('.toast-root');
-    if (toasts.length > 0) {
-        toasts.forEach(toast => toast.remove());
-        console.log("Warning toast removed!");
-        clearInterval(myInterval);
-    } else {
-        console.log("tick");
+// @name         Warning Toast Remover 9002
+// @match        https://ylilauta.org/*
+// @grant        none
+// ==/UserScript==
+(function() {
+    "use strict"
+
+    const myInterval = setInterval(ToastClean, 1000);
+
+    function ToastClean() {
+        const toasts = document.querySelectorAll('.toast-root');
+        if (toasts.length > 0) {
+            toasts.forEach(toast => toast.remove());
+            console.log("Warning toast removed! t. WTR9002");
+            clearInterval(myInterval);
+        } else {
+            console.log("tick");
+        }
     }
-}
+
+})()
